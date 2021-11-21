6978cac73572068031f0203d4e001d8147119582
Kuba Biel <jakubiel@o2.pl>
2021-11-21 12:42:47 +0100

    ham3

--- 08 hamburger/03script.js
+++ 08 hamburger/03script.js
@@ -7,19 +7,23 @@ const bars = document.querySelector('.fa-bars')
 
 const iconX = document.querySelector('.fa-times')
 
-//1.1 element properties
-
-const mLeft = 0
-
 //2. function
 
 function move() {
-  aside.classList.toggle('move')
+  aside.classList.toggle('show')
   bars.classList.toggle('show')
   iconX.classList.toggle('show')
-
 }
 
 //3. listener
 
 btn.addEventListener('click', move)
+
+
+
+
+// jQ code:
+
+// $(".burger").on("click", function () {
+//   $(".fas, aside").toggleClass("show");
+// })
