1c0dffda77ae6d44b84cbc17a965cae9416ae690
Kuba Biel <jakubiel@o2.pl>
2021-11-21 14:46:48 +0100

    ham4

--- 08 hamburger/04script.js
+++ 08 hamburger/04script.js
@@ -1,28 +1,38 @@
-//1. elements
+// //1. elements
+// const btn = document.querySelector('.burger')
+
+// const aside = document.querySelector('aside')
+
+// const bars = document.querySelector('.fa-bars')
+
+// const iconX = document.querySelector('.fa-times')
+
+// const parag = document.querySelector('.wrapper')
+
+// //2. function
+
+// function move() {
+//   aside.classList.toggle('show')
+//   bars.classList.toggle('show')
+//   iconX.classList.toggle('show')
+//   parag.classList.toggle('show')
+// }
+
+// //3. listener
+
+// btn.addEventListener('click', move)
+
+//JS shorter
+
 const btn = document.querySelector('.burger')
+const activeElement = document.querySelectorAll('.active');
 
-const aside = document.querySelector('aside')
-
-const bars = document.querySelector('.fa-bars')
-
-const iconX = document.querySelector('.fa-times')
-
-const parag = document.querySelector('.wrapper')
-
-//2. function
-
-function move() {
-  aside.classList.toggle('show')
-  bars.classList.toggle('show')
-  iconX.classList.toggle('show')
-  parag.classList.toggle('show')
-}
-
-//3. listener
-
-btn.addEventListener('click', move)
-
-
+btn.addEventListener('click', function () {
+  console.log('ok');
+  for (let i = 0; i < activeElement.length; i++) {
+    activeElement[i].classList.toggle('show')
+  }
+})
 
 
 // jQ code:
